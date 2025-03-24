c4bbf672031f0169a45335d3a6340e369a350b75
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-03-24 20:28:26 -0300

    terminado batalha-naval-novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,69 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+void imprimirTabuleiroDeDez(int matriz[10][10]){
+
+    printf("\n  "); // espaço para alinhas as LETRAS com os ZEROS.
+
+    for(char letra = 'A'; letra <= 'J'; letra++){ // Loop para printar as letras das colunas.
+        printf("%2c", letra);
+    }
+
+    printf("\n"); // Quebra de linha para que as letras fiquem em cima dos ZEROS.
+
+    for(int i = 0; i < 10; i++){ // loop para printar os numeros das linhas
+        printf("%2d ", i + 1);
+   
+        for(int j = 0; j < 10; j++){ // loop para printar a matriz, após o numero da linha. correndo linhas e colunas.
+          printf("%d ", matriz[i][j]);
+       }
+
+        printf("\n");
+    }
+
+}
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    // NAVIO = 3
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    int tabuleiro[10][10] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
+        {0, 0, 0, 0, 3, 0, 0, 0, 0 ,0},
+        {0, 0, 0, 0, 3, 0, 0, 0, 0 ,0},
+        {0, 0, 0, 0, 3, 0, 0, 0, 0 ,0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
+        {0, 0, 0, 0, 0, 0, 3, 3, 3 ,0}
+    };
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
 
+   imprimirTabuleiroDeDez(tabuleiro); // Chama a função para imprimir o Tabuleiro
+
+    
+
+   printf("\n");
     return 0;
 }
+
+/*
+
+    SAIDA ESPERADA ! NOVATO
+
+    A B C D E F G H I J
+  1 0 0 0 0 0 0 0 0 0 0
+  2 0 0 0 0 0 0 0 0 0 0
+  3 0 0 0 0 0 0 0 0 0 0 
+  4 0 0 0 0 0 0 0 0 0 0
+  5 0 0 0 0 0 0 0 0 0 0
+  6 0 0 0 0 0 0 0 0 0 0
+  7 0 0 0 0 0 0 0 0 0 0
+  8 0 0 0 0 0 0 0 0 0 0
+  9 0 0 0 0 0 0 0 0 0 0
+ 10 0 0 0 0 0 0 0 0 0 0
+
+
+*/
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+
+    // for(char letra = 'A'; letra <= 'J'; letra++){ // Loop para printar as letras das colunas.
+    //     printf("%2c", letra);
+    // }
