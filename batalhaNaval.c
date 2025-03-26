2dfa536d0dcdba7a18a36b50ccea260d6b3d5baa
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-03-25 21:52:58 -0300

    terminado batalha-naval intermediario

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-void imprimirTabuleiroDeDez(int matriz[10][10]){
+#define linhas 10
+#define colunas 10
+
+void imprimirTabuleiroDeDez(int matriz[linhas][colunas]){
     
     printf("\n  "); // espaço para alinhas as LETRAS com os ZEROS.
     
@@ -10,52 +13,53 @@ void imprimirTabuleiroDeDez(int matriz[10][10]){
     
     printf("\n"); // Quebra de linha para que as letras fiquem em cima dos ZEROS.
     
-    for(int i = 0; i < 10; i++){ // loop atribiu valores aos indices.
-        for(int j = 0; j < 10; j++){
+    for(int i = 0; i < linhas; i++){
+        printf("%2d ", i + 1); // printa os numeros antes dos ZEROS.
+
+        for(int j = 0; j < colunas; j++){ // loop atribiu valores aos indices.
             matriz[i][j] = 0;
-            printf("%d %d ", matriz[i][j]);
+
+            for(int k = 3; k < 6; k++){ // injeta o 'navio' na linha 5 da matriz.
+                matriz[4][k] = 3;
+            }
+
+            for(int l = 7; l < 10; l++){ // Injeta o 'navio' na coluna 'I' e nas linhas 8, 9, 10.
+                matriz[l][8] = 3;
+            }
+
+            for(int m = 0; m < 3; m++){ // Injeta o navio na Horizontal, começa em [0,0].
+                matriz[m][m] = 3;
+            }
+
+            for(int n = 0; n < 3; n++){ // Injeta o navio na Horizontal, começa em [0,0].
+                matriz[n][n] = 3;
+            }
+
+            for(int o = 0 ; o < 3 ; o++){ // injeta o navio na diagonal [7,0]
+               matriz[8 - o][0 + o] = 5;
+            }
+            
+            printf("%d ", matriz[i][j]);
         }
+
         printf("\n");
     }
-    // printf("%2d ", i + 1);
-    // for(int i = 0; i < 10; i++){// { loop para printar os numeros das linhas
-    
-    // for(int j = 0; j < 10; j++){ // loop para printar a matriz, após o numero da linha. correndo linhas e colunas.
-    // printf("%d ", matriz[i][j]);
-    // }
-    
-    //     printf("\n");
-    // }
-    
+
 }
 
 int main() {
     
-    // NAVIO = 3
+    int tabuleiro[linhas][colunas];
     
-    // int tabuleiro[10][10] = {
-        //     {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
-        //     {0, 0, 0, 0, 3, 0, 0, 0, 0 ,0},
-        //     {0, 0, 0, 0, 3, 0, 0, 0, 0 ,0},
-        //     {0, 0, 0, 0, 3, 0, 0, 0, 0 ,0},
-        //     {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
-        //     {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
-        //     {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
-        //     {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
-        //     {0, 0, 0, 0, 0, 0, 3, 3, 3 ,0}
-        // };
-        
-        int tabuleiro[10][10];
-        printf("\n");
+    // Espaço para melhor tabulação
+    printf("\n"); 
 
+    // Chama a função para imprimir o tabuleiro
+    imprimirTabuleiroDeDez(tabuleiro);
 
-
-   imprimirTabuleiroDeDez(tabuleiro); // Chama a função para imprimir o Tabuleiro
-
+    // Espaço para melhor tabulação.
+    printf("\n");
     
-
-   printf("\n");
-   
     return 0;
 }
 
